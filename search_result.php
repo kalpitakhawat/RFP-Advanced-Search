d58fe2ddb2454661507bb0ce83ba91b80499640b
Kalpit Akhawat <kalpitakhawat@gmail.com>
2018-03-29 23:56:00 +0530

    Switch

--- search_result.php
+++ search_result.php
@@ -1,4 +1,63 @@
 <?php include("header.php"); ?>
+<style type="text/css">
+  .switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {display:none;}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
 <!-- Page Content -->
 <div class="container" id="app">
   <div class="row">
@@ -23,12 +82,23 @@
                   </div>
                 </div>
               </form>
+
+             <div class="row mt-5">
+                <div class="col-8 text-right"><h5>Show Filters</h5></div>
+                <div class="col-4">
+                  <!-- Rounded switch -->
+              <label class="switch">
+                <input type="checkbox" v-model="filter">
+                <span class="slider round"></span>
+              </label>
+                </div>
+             </div>
             </div> <!-- card-body.// -->
           </div>
         </article> <!-- card-group-item.// -->
       </div> <!-- card.// -->
     </div>
-     <div class="col-md-3">
+     <div class="col-md-3" v-if="filter" >
       <div class="card">
         <article class="card-group-item">
           <header class="card-header">
@@ -67,7 +137,7 @@
         </article> <!-- card-group-item.// -->
       </div> <!-- card.// -->
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3" v-if="filter">
       <div class="card">
         <article class="card-group-item">
           <header class="card-header">
@@ -100,7 +170,7 @@
         </article> <!-- card-group-item.// -->
       </div> <!-- card.// -->
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3" v-if="filter">
       <div class="card">
        <article class="card-group-item">
           <header class="card-header">
@@ -173,6 +243,7 @@
     cert:'',
     query:'',
     result:'',
+    filter:true,
   },
   methods:{
     search:function () {
@@ -180,49 +251,53 @@
       if(self.textInput != ''){
         self.q = self.textInput;
       }
-      self.range = ''
-      if(self.priceRange.length > 0){
-        
-        self.priceRange.forEach(function (item , index) {
-          if(index != 0){
-            self.range += ' OR '
-          }else{
-            self.range = ''
-            self.range += ' AND ('
-          }
-          if (item == 0) {
-            self.range += '(cost < 100)'
-          } else if (item == 1) {
-            self.range += '(cost > 101 AND cost < 200)'
-          } else if (item == 2) {
-            self.range += '(cost > 201 AND cost < 300)'
-          } else if (item == 3) {
-            self.range += '(cost > 300)'
-          }
-        });
-        self.range +=')'
-      }
-      self.rating = ''
-      if(self.userRating.length >0){
-        // self.rating = '('
-        self.userRating.forEach(function (item ,index) {
-          if(index != 0){
-            self.rating += ' OR '
-          }else if(index == 0){
-            self.rating += ' AND ( '
-          }
-          self.rating += 'rating >= ' + item
-        });
-        self.rating = self.rating + ')'
-      }
+      self.query = ''
+      if(self.filter){
+        self.range = ''
+        if(self.priceRange.length > 0){
+          
+          self.priceRange.forEach(function (item , index) {
+            if(index != 0){
+              self.range += ' OR '  
+            }else{
+              self.range = ''
+              self.range += ' AND ('
+            }
+            if (item == 0) {
+              self.range += '(cost < 100)'
+            } else if (item == 1) {
+              self.range += '(cost > 101 AND cost < 200)'
+            } else if (item == 2) {
+              self.range += '(cost > 201 AND cost < 300)'
+            } else if (item == 3) {
+              self.range += '(cost > 300)'
+            }
+          });
+          self.range +=')'
+        }
+        self.rating = ''
+        if(self.userRating.length >0){
+          // self.rating = '('
+          self.userRating.forEach(function (item ,index) {
+            if(index != 0){
+              self.rating += ' OR '
+            }else if(index == 0){
+              self.rating += ' AND ( '
+            }
+            self.rating += 'rating >= ' + item
+          });
+          self.rating = self.rating + ')'
+        }
 
-      if(self.certified == 'yes'){
-        self.cert = ' AND certificate != ""'
-      }else if (self.certified == 'no') {
-        self.cert = ' AND certificate == ""'
+        if(self.certified == 'yes'){
+          self.cert = ' AND certificate != ""'
+        }else if (self.certified == 'no') {
+          self.cert = ' AND certificate == ""'
+        }
+        self.query = '';
+        self.query = self.range+self.rating+self.cert
+
       }
-      self.query = '';
-      self.query = self.range+self.rating+self.cert
       console.log(self.query);
 
        $.post('ajax_search_result.php' , {
@@ -251,6 +326,10 @@
       self.search();
     },
     certified:function () {
+      var self = this;
+      self.search();
+    },
+    filter:function () {
       var self = this;
       self.search();
     }
